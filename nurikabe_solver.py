cf6c8c1f1b30c866cd599bd938432f0114879ef7
Jacek Wikiera <jf@wikiera.net>
2019-06-20 11:28:38 +0200

    Merged with validite.py, cleaned up
    validite.py has been finished and integrated into the main solver. It has three functions that let check if a Nurikabe is solved or not: island integrity check, wall integrity check and detection of 2x2 blocks int the wall.

--- nurikabe_solver.py
+++ nurikabe_solver.py
@@ -1,29 +1,21 @@
-# Nurikabe solver part
-# By Jacek Wikiera
-# last edit 27 april 19 - 22:45
-# last edit 10 June 19 (Eric)
-
 import sys, math
-
-# check if tile is an int (just a float function changed for name)
+#Function to check if tile is an int (just a float function changed for name)
 def isInt(table, x, y):
-    val = True
+    flag = True
     try:
         int(table[x][y])
     except ValueError:
-        val = False
-    return val
+        flag = False
+    return flag
 
-
-# check if two given tiles are touching
+#Function to check if two given tiles are touching
 def areTouching(x1, y1, x2, y2):
     touching = False
-    if (abs(x1 - x2) == 1 and y2 == y1) or (x2 == x1 and abs(y1 - y2) == 1):
+    if (abs(x1-x2) == 1 and y2 == y1) or (x2 == x1 and abs(y1-y2) == 1):
         touching = True
     return touching
 
-
-# check continuity of the wall
+# **OWN** **NOT IN USE** Function to check continuity of the wall
 def checkWallIntegrity(table):
     setList = []
     for i in range(x_len):
@@ -41,135 +33,34 @@ def checkWallIntegrity(table):
                             break
                         l += 1
                 if not found:
-                    setList.append([(i, j)])
-
-    print(setList)
+                    setList.append([(i, j)]) #print(setList)
     for i in range(len(setList)):
-        setList[i] = set(setList[i])
-    print(setList)
-
+        setList[i] = set(setList[i]) #print(setList)
     if (len(setList[0] & setList[1])) > 0:
-        print("hello")
-
+        pass #print("hello")
     i = 0
-    j = 0
     while i < len(setList) - 1:
         j = 1
         while j < len(setList):
-            if (len(setList[i] & setList[j])) > 0 and not (setList[i] == setList[j]):
+            if (len(setList[i] & setList[j])) > 0 and not(setList[i] == setList[j]):
                 setList[i] = setList[i] | setList[j]
                 del setList[j]
                 j = 1
             else:
                 j += 1
         i += 1
+    #verify if there is one or more sets
+    #print(setList)
+    if len(setList) > 1: #print("The wall is not continuous")
+        return True
+    else: #print("The wall is continuous")
+        return False
 
-    print(setList)
-    if len(setList) > 1:
-        print("La mer n'est pas continue")
-    else:
-        print("La mer est continue")
-
-
-# turn tiles between numbers black "B"
-def elimAdj(table):
-    # set x and y length of the table
-    x_len = len(table)
-    y_len = len(table[0])
-    
-    for i in range(x_len):
-        for j in range(y_len):
-            # check in the right direction
-            if (i < (x_len - 2)) and isInt(table, i, j) and isInt(table, i + 2, j):
-                table[i + 1][j] = "B"
-            # check below
-            if (j < (y_len - 2)) and isInt(table, i, j) and isInt(table, i, j + 2):
-                table[i][j + 1] = "B"
-    return table
-
-
-# turns tiles next to "ones" black ("B")
-def elimAroundOnes(table):
-    
-    # set x and y length of the table
-    x_len = len(table)
-    y_len = len(table[0])
-    
-    for i in range(x_len):
-        for j in range(y_len):
-            if table[i][j] == "1":
-                if i > 0:  # left
-                    table[i - 1][j] = "B"
-                if j > 0:  # up
-                    table[i][j - 1] = "B"
-                if i < x_len - 1:  # right
-                    table[i + 1][j] = "B"
-                if j < y_len - 1:  # down
-                    table[i][j + 1] = "B"
-    return table
-
-
-# turns tiles in diagonal black ("B")
-def diagonal(table):
-    # set x and y length of the table
-    x_len = len(table)
-    y_len = len(table[0])
-    
-    for i in range(x_len):
-        for j in range(y_len):
-            # case n1: "2" "w"
-            #          "w" "2"
-            if i < x_len - 1 and j < y_len - 1:
-                if isInt(table, i, j) and isInt(table, i + 1, j + 1):
-                    table[i + 1][j] = "B"
-                    table[i][j + 1] = "B"
-                # case n2: "w" "2"
-                #          "2" "w"
-                if isInt(table, i + 1, j) and isInt(table, i, j + 1):
-                    table[i][j] = "B"
-                    table[i + 1][j + 1] = "B"
-    return table
-
-
-# output table in console
-def printTable(table):
-    tempStr = ""
-    for i in range(y_len):
-        for j in range(x_len):
-            tempStr += table[j][i] + " "
-        print(tempStr)
-        tempStr = ""
-
-"""
-# old table
-table = ["w", "1", "w", "w",
-         "w", "w", "w", "2",
-         "1", "w", "2", "w",
-         "w", "w", "w", "w",
-         "w", "w", "w", "w",
-         "2", "w", "2", "w"]
-# new table
-table = [["w", "w", "1", "w", "w", "2"],
-         ["1", "w", "w", "w", "w", "w"],
-         ["w", "w", "2", "w", "w", "2"],
-         ["w", "2", "w", "w", "w", "w"]]"""
-
-
-
-"""# execute functions
-table = elimAroundOnes(table)
-table = elimAdj(table)
-table = diagonal(table)
-checkWallIntegrity(table)
-
-printTable(table)"""
-
+#**FROM THE BOOK** **IN USE**
 # Wall Integrity Algorithm
-
 #Algorithm which checks whether the wall of a Nurikabe is continuous or not. It is based on an algorithm from the book: "Foundation of Computer Science in C" by Alfred V. Aho and Jeffrey D. Ullman.
 #The original example was written in C and has been adapted to the Nurikabe. The original algorithm can be found here: http://blough.ece.gatech.edu/3020/focs.pdf on pages 474 - 475
 #By Jacek Wikiera - Sat 27 april 19 - 22:11
-
 
 # Check whether the wall of the Nurikabe is continuous or not
 def checkWallIntegrity2(table):
@@ -274,9 +165,147 @@ def checkWallIntegrity2(table):
                 
         if len(rootList) > 1:
             print("La mer n'est pas continue")
+            return False
         else:
             print("La mer est continue")
+            return True
     # If there is no wall cells at all
     else:
         print("Il n'y a pas de mer")
+        return None
 
+#Function to turn tiles between numbers black "B"
+def elimAdj(table):
+    for i in range(x_len):
+        for j in range(y_len):
+            #check in the right direction
+            if (i < (x_len-2)) and isInt(table, i,j) and isInt(table, i+2,j):
+                table[i+1][j] = "B"
+            #check below
+            if (j < (y_len-2)) and isInt(table, i,j) and isInt(table, i,j+2):
+                table[i][j+1] = "B"
+    return table
+
+#Function to turn tiles next to "ones" black ("B")
+def elimAroundOnes(table):
+    for i in range(x_len):
+        for j in range(y_len):
+            if table[i][j] == "1":
+                if i > 0: #left
+                    table[i-1][j] = "B"
+                if j > 0: #up
+                    table[i][j-1] = "B"
+                if i < x_len-1: #right
+                    table[i+1][j] = "B"
+                if j < y_len-1: #down
+                    table[i][j+1] = "B"
+    return table
+
+#Function to turn tiles in diagonal black ("B")
+def diagonal(table):
+    for i in range(x_len):
+        for j in range(y_len):
+            # case n°1: "2" "w"
+            #           "w" "2"
+            if i < x_len-1 and j < y_len-1:
+                if isInt(table, i,j) and isInt(table, i+1,j+1):
+                    table[i+1][j] = "B"
+                    table[i][j+1] = "B"
+            # case n°1: "w" "2"
+            #           "2" "w"
+                if isInt(table, i+1,j) and isInt(table, i,j+1):
+                    table[i][j] = "B"
+                    table[i+1][j+1] = "B"
+    return table
+
+#Function to check for 2x2 wall blocks
+def wallBlockCheck(table):
+    x_len = len(table)
+    y_len = len(table[0])
+    foundBlock = False
+    for i in range(x_len-1):
+        for j in range(y_len-1):
+            if table[i][j] == "B" and table[i+1][j] == "B" and table[i][j+1] == "B" and table[i+1][j+1] == "B":
+                foundBlock = True #print("2x2 wall block found at x:",i,"and y:",j)
+                return (i, j)
+    if not foundBlock: #print("No 2x2 blocks in the wall")
+        return None
+        
+#Function to check if a single island is complete
+tempTable = []
+revertTable = []
+def islandCheck(x, y, table, counter, returning = False):
+    if (x, y) not in tempTable:
+        counter = counter-1 #print("counter is",counter)
+        if counter == 0:
+            print("Island complete")
+            return True
+        tempTable.append((x, y))
+    if not returning:
+        revertTable.append((x, y))
+    if x > 0 and table[x-1][y] == "I" and (x-1, y) not in tempTable: #print("left")
+        return islandCheck(x-1, y, table, counter, returning = False)
+    elif y > 0 and table[x][y-1] == "I" and (x, y-1) not in tempTable: #print("up")
+        return islandCheck(x, y-1, table, counter, returning = False)
+    elif (x < x_len-1) and table[x+1][y] == "I" and (x+1, y) not in tempTable: #I wonder if the < x_len-1 works in all cases ###print("right")
+        return islandCheck(x+1, y, table, counter, returning = False)
+    elif (y < y_len-1) and table[x][y+1] == "I" and (x, y+1) not in tempTable: #print("down")        
+        return islandCheck(x, y+1, table, counter, returning = False)
+    elif len(revertTable) > 1:
+        revertTable.pop() #print("returning")
+        return islandCheck(revertTable[len(revertTable)-1][0], revertTable[len(revertTable)-1][1], table, counter, returning = True)
+    else:
+        print("Island not complete")
+        return False
+
+#Function to check if all islands are complete
+def allIslCheck(table):
+    flag = True
+    for i in range(x_len):
+        for j in range(y_len):
+            if isInt(table, i, j):
+                tempTable.clear()
+                revertTable.clear()
+                counter = int(table[i][j])
+                if flag:
+                    flag = islandCheck(i, j, table, counter)
+                    revertTable
+    return flag
+
+#Function to display table in console
+def printTable(table):
+    tempStr = ""
+    for i in range(y_len):
+        for j in range(x_len):
+            tempStr += table[j][i]+" "
+        print(tempStr)
+        tempStr = ""
+
+#tables
+table =[["B", "B", "1", "B", "I", "2"],
+        ["1", "B", "B", "B", "B", "B"],
+        ["B", "B", "2", "I", "B", "2"],
+        ["I", "2", "B", "B", "B", "I"]]
+
+table = [["1", "B", "2", "I", "B", "2", "I"],
+         ["B", "B", "B", "B", "B", "B", "B"],
+         ["2", "I", "B", "4", "I", "B", "1"],
+         ["B", "B", "I", "I", "B", "B", "B"],
+         ["2", "B", "B", "B", "2", "I", "B"],
+         ["I", "B", "4", "B", "B", "B", "B"],
+         ["B", "B", "I", "I", "I", "B", "1"]]
+
+#set x and y length of the table
+x_len = len(table)
+y_len = len(table[0])
+
+##execute functions
+#table = elimAroundOnes(table)
+#table = elimAdj(table)
+#table = diagonal(table)
+#block_coord = wallBlockCheck(table)
+#print(block_coord[1])
+#checkWallIntegrity2(table)
+print(allIslCheck(table))
+
+printTable(table)
