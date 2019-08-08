79ba88aaf62432aef8e3cef5c82df787416aed16
Eric Holzer <eric.holzer@citycable.ch>
2019-08-08 20:44:12 +0200

    Added Grid scaling according to the table shape

--- nurikabe_main.py
+++ nurikabe_main.py
@@ -1,6 +1,7 @@
 # Nurikabe Display - Version 2.0 (Object Oriented Programming Version)
 # Author : Eric Holzer
-# Date : 28 June 2019
+# Created : 28 June 2019
+# Last Edit : 8 August 2019
 
 # Import modules
 import pygame
@@ -18,12 +19,39 @@ BLUE  = (0, 0, 255)
 GREEN = (0, 255, 0)
 
 # Define table
-table = np.array([[0, 1, 0, 0],
+table = None
+
+table1 = np.array([[0, 1, 0, 0],
                   [0, 0, 0, 2],
                   [1, 0, 2, 0],
                   [0, 0, 0, 0],
                   [0, 0, 0, 0],
                   [2, 0, 2, 0]])
+
+table2 = np.array([[0, 0, 0, 0, 0],
+                   [2, 0, 0, 1, 0],
+                   [0, 0, 0, 0, 0],
+                   [0, 2, 0, 0, 0],
+                   [0, 0, 0, 0 ,2]])
+
+table3 = np.array([[0, 0, 0, 0, 0],
+                   [0, 0, 0, 1, 0],
+                   [0, 0, 3, 0, 0],
+                   [1, 0, 0, 0, 0],
+                   [0, 0, 4, 0, 0]])
+
+table4 = np.array([[1, 0, 2, 0, 2, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0],
+                   [2, 0, 0, 0, 0, 4, 0],
+                   [0, 0, 4, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 2, 0, 0],
+                   [2, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 1, 0, 0, 0, 1]])
+
+tableTest = np.zeros((10, 20), dtype=int)
+
+table = tableTest
+table[5, 5] = 1
 
 # Define classes
 class App:
@@ -38,7 +66,7 @@ class App:
         pygame.font.init()
                 
         # Create screen
-        self.screen_size      = (800, 800) # Default screen size
+        self.screen_size      = (750, 750) # Default screen size
         self.screen_center    = (self.screen_size[0] / 2, self.screen_size[1] / 2)
         self.background_color = WHITE
         self.title            = "Nurikabe"
@@ -58,7 +86,7 @@ class App:
         solve = Button('SOLVE', size=72, cmd='App.room = App.rooms[1]')
         solve.rect.center = (self.screen_center[0], self.screen_center[1] + 100)
         
-        option = Button('OPTION', size=72, cmd='print("OPTION ROOM)')
+        option = Button('OPTION', size=72, cmd='print("OPTION ROOM")')
         option.rect.center = (self.screen_center[0], self.screen_center[1] + 200)
         
         credit = Text('Made by Eric Holzer and Jacek Wikiera, 2019')
@@ -66,21 +94,24 @@ class App:
         
         # Room 1 (Choosing Level Room)
         Room()
-        Button('menu', cmd='App.room = App.rooms[0]', pos=(20, 20), thickness = 3)
-        Text('Choose a level', pos=(300, 0), size=72)
-        Button('1', pos=(100, self.screen_center[1]), size=72, cmd='App.room = App.rooms[2];print(1)')
-        Button('2', pos=(200, self.screen_center[1]), size=72, cmd='print(2)')
+        Button('menu', cmd='App.room = App.rooms[0]', pos=(10, 10), thickness = 2, inf = 10)
+        text = Text('Choose a level', pos=(0, 0), size=72)
+        text.rect.center = (self.screen_center[0], 30)
+        Button('1', pos=(100, self.screen_center[1]), size=72, cmd='App.room = App.rooms[2];App.grid.set_table(table1);App.grid.playable = True')
+        Button('2', pos=(200, self.screen_center[1]), size=72, cmd='App.room = App.rooms[2];App.grid.set_table(table2)')
         Button('3', pos=(300, self.screen_center[1]), size=72, cmd='print(3)')
         Button('4', pos=(400, self.screen_center[1]), size=72, cmd='print(4)')
         
         # Room 2 (Playable Room)  
         Room()
-        Button('menu', cmd='App.room = App.rooms[0]', pos=(10, 10))
-        Grid(table)
-        Text('Mode: play', pos=(300, 0), size=30)
-        Button('check_continuity', pos=(600, 100), size=20, cmd='print("check_continuity")')
-        Button('2x2 block', pos=(600, 200), size=20, cmd='print("2x2_block")')
-        Button('island_complete', pos=(600, 300), size=20, cmd='print("island_complete")')
+        Button('menu', cmd='App.room = App.rooms[0]', pos=(10, 10), thickness = 2, inf = 10)
+        App.grid = Grid(table)
+        App.grid.playable = True
+        mode = Text('Mode: play', size=30)
+        mode.rect.center = (self.screen_center[0], 20)
+        Button('check_continuity', pos=(600, 100), size=20, cmd='print("check_continuity")', inf = 20, thickness = 2)
+        Button('2x2 block', pos=(600, 150), size=20, cmd='print("2x2_block")', inf = 20, thickness = 2)
+        Button('island_complete', pos=(600, 200), size=20, cmd='print("island_complete")', inf = 20, thickness = 2)
         
         App.room = App.rooms[0] # Set the first room to Title Screen Room
         
@@ -99,10 +130,16 @@ class App:
                     if event.key == K_ESCAPE:
                         self.active = False
                         
+                elif event.type == MOUSEBUTTONDOWN:
+                    print(event.pos)
+                        
                 App.room.do_event(event)
             
             # Draw section
             self.draw()
+            
+            #if App.room == App.rooms[0]:
+               # print(1)
                             
         pygame.quit()
         exit()
@@ -151,13 +188,14 @@ class Text:
 class Button(Text):
     """Create a button object."""
     
-    def __init__(self, text, size=24, col=BLACK, pos=(0, 0), cmd='', thickness=4):
+    def __init__(self, text, size=24, col=BLACK, pos=(0, 0), cmd='', thickness=4, inf=40):
         super().__init__(text, size, col, pos)
         self.cmd = cmd
         self.thickness = thickness
-    
+        self.inf = inf # inflation of the rectangle around the text
+        
     def draw(self):
-        r = self.rect.inflate(40, 40) # return a new rectangle that is bigger
+        r = self.rect.inflate(self.inf, self.inf) # return a new rectangle that is bigger
         pygame.draw.rect(App.screen, self.col, r, self.thickness)
         super().draw()
         
@@ -185,26 +223,38 @@ class Room:
                 if object.rect.collidepoint(event.pos):
                     object.do_event(event)
                     if event.type == MOUSEBUTTONDOWN:
-                        print('mouse click in', object) 
+                        print('mouse click in', object)
+                        
                         
 
 class Grid:
     """Create a grid object."""
     
-    def __init__(self, table, pos=(50, 50), case_length=50, col=BLACK, thickness=2):
+    def __init__(self, table, pos=(50, 50), col=BLACK):
+        self.set_table(table, pos, col)
+        App.room.objects.append(self)
+        
+    def set_table(self, table, pos=(50, 50), col=BLACK):
         n, m = table.shape
         self.n = n # Number of rows
         self.m = m # Number of columns
         self.pos = pos
         self.table = table
-        self.case_length = case_length
         self.col = col
-        self.thickness = thickness
-        self.rect = Rect(*pos, m*case_length, n*case_length)
         self.selected_cell_pos = 0, 0
-        self.create_cell_text()
+        self.cursor_on_grid = False
+        self.playable = False
         
-        App.room.objects.append(self)
+        # Scale the case_length according to the number of rows and columns
+        if self.n > self.m:
+            self.case_length = 650 // self.n
+        else:
+            self.case_length = 500 // self.m
+            
+        self.rect = Rect(*pos, m*self.case_length, n*self.case_length)
+        self.create_cell_text()
+        self.thickness = (self.case_length * 4) // 100 # Scaled according to the case_length
+        
     
     def create_cell_text(self):
         for i in range(self.n):
@@ -212,8 +262,10 @@ class Grid:
                 current_rect = self.get_cell_rect(i, j)
                 
                 # Draw the number
+                number_size = (self.case_length * 60) // 100 # Scaled according to the case_length
+                
                 if self.table[i, j] > 0: 
-                    text = Text(str(table[i, j]), size=40)
+                    text = Text(str(self.table[i, j]), size = number_size)
                     text.rect.center = current_rect.center
         
     def draw(self):
@@ -231,7 +283,7 @@ class Grid:
             x = x0 + i * self.case_length
             pygame.draw.line(App.screen, self.col, (x, y0), (x, y1), self.thickness)
         
-        # Draw numbers and color on the grid
+        # Draw colors on the grid
         for i in range(self.n):
             for j in range(self.m):
                 current_rect = self.get_cell_rect(i, j)
@@ -242,12 +294,30 @@ class Grid:
                     
                 # Draw the cell in white (little black square)
                 elif self.table[i, j] == -2:
-                    current_rect.inflate_ip(-80, -80) # Shrink the current rectangle
-                    pygame.draw.rect(App.screen, self.col, current_rect, 0)
+                    inf = (self.case_length * -80) // 100 # Scaled according to the case_length
                     
-        # Draw the selected cell
-        selected_cell_rect = self.get_cell_rect(*self.selected_cell_pos)
-        pygame.draw.rect(App.screen, RED, selected_cell_rect, self.thickness)
+                    current_rect.inflate_ip(inf, inf) # Shrink the current rectangle
+                    pygame.draw.rect(App.screen, self.col, current_rect, 0)
+        
+        # If PLAY Room
+        if self.playable:
+            # Check if the mouse is colliding the grid
+            if pygame.mouse.get_pos()[0] < x0:
+                self.cursor_on_grid = False
+            elif pygame.mouse.get_pos()[0] > (self.m)*self.case_length+x0:
+                self.cursor_on_grid = False
+            elif pygame.mouse.get_pos()[1] < y0:
+                self.cursor_on_grid = False
+            elif pygame.mouse.get_pos()[1] > (self.n)*self.case_length+y0:
+                self.cursor_on_grid = False
+            else:
+                self.cursor_on_grid = True
+                        
+            # Draw the selected cell
+            selected_cell_rect = self.get_cell_rect(*self.selected_cell_pos)
+            
+            if self.cursor_on_grid:
+                pygame.draw.rect(App.screen, RED, selected_cell_rect, self.thickness)
                     
     def get_cell_rect(self, i, j):
         """Get rectangle from index (i, j)."""
@@ -266,23 +336,26 @@ class Grid:
         return i, j
             
     def do_event(self, event):
-        i, j = self.get_cell_index(event.pos)
         
-        # Move the selected cell (red square)
-        self.selected_cell_pos = i, j
-        
-        if event.type == MOUSEBUTTONDOWN:
+        # If PLAY Room
+        if self.playable:
+            i, j = self.get_cell_index(event.pos)
             
-            print(i, j, self.table[i, j])
+            # Move the selected cell (red square)
+            self.selected_cell_pos = i, j
             
-            if self.table[i, j] == 0: # If the cell is undefined turn it to black
-                self.table[i, j] = -1
+            if event.type == MOUSEBUTTONDOWN:
                 
-            elif self.table[i, j] == -1: # If the cell is black turn it to white
-                self.table[i, j] = -2
+                print(i, j, self.table[i, j])
                 
-            elif self.table[i, j] == -2: # If the cell is white turn it to undefined
-                self.table[i, j] = 0
+                if self.table[i, j] == 0: # If the cell is undefined turn it to black
+                    self.table[i, j] = -1
+                    
+                elif self.table[i, j] == -1: # If the cell is black turn it to white
+                    self.table[i, j] = -2
+                    
+                elif self.table[i, j] == -2: # If the cell is white turn it to undefined
+                    self.table[i, j] = 0
 
 # Run the program
 if __name__ == '__main__':
